5ede7144aa5b6bd338274f2e12666c1e68511b1e
vikash-98 <vikashb4782@gmail.com>
2025-01-31 13:47:34 +0530

    update

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -1,11 +1,28 @@
-import { createContext } from "react";
+/* eslint-disable react/prop-types */
+import { createContext, useState } from "react";
 import { food_list } from "../../assets/assets";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const storeContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItem, setCartItem] = useState({});
+  const addToCart = (itemId) => {
+    if (!cartItem[itemId]) {
+      setCartItem((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+  const removeFromCart = (itemId) => {
+    setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
   const contextValue = {
     food_list,
+    cartItem,
+    setCartItem,
+    removeFromCart,
   };
   return (
     <storeContext.Provider value={contextValue}>
@@ -13,4 +30,5 @@ const StoreContextProvider = (props) => {
     </storeContext.Provider>
   );
 };
+
 export default StoreContextProvider;
